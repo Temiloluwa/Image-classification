9ba45fecf1fc4e63aa71064ba6482334adbfc589
Temiloluwa <temilolu74@yahoo.co.uk>
2021-05-20 10:29:48 +0200

    included onnx model

--- run_inference.py
+++ run_inference.py
@@ -1,11 +1,11 @@
 import os
 import torch
 import numpy as np
-import onnx
+import onnxruntime as ort
 import argparse
 from torchvision import models
 from data import preprocess_data
-from utils import read_imagenet_classnames
+from utils import read_imagenet_classnames, softmax, export_to_onnx
 
 parser = argparse.ArgumentParser(description='Inference Trained Model')
 parser.add_argument('data', metavar='DIR', help='default data path')
@@ -14,19 +14,28 @@ parser.add_argument('-tp', '--top-predictions', metavar='NUMPRED',\
                      default=5, help='number of top predictions per sample')
 parser.add_argument('-mt', '--model-type', metavar='MODELTYPE', choices=['pytorch', 'onnx'],\
                     default='pytorch', help='onnx or pytorch')
+parser.add_argument('-exp', '--export-model', type=bool,\
+                    default=False, help='export model to onnx')
 
 
 def run_inference(model, data, onnx = False):
     input_data, _ = data
-    predictions = model(input_data)
     if not onnx:
+        predictions = model(input_data)
         probabilities = torch.softmax(predictions, 1)
         pred_indices = torch.argsort(probabilities, 1,\
                             descending=True)[:, : args.top_predictions]
         probabilities = torch.gather(probabilities, 1, pred_indices)
         probabilities = (probabilities * 100).detach().numpy()
         pred_indices = pred_indices.detach().numpy()
-
+    else:
+        input_data = input_data.detach().numpy()
+        input_name = model.get_inputs()[0].name
+        predictions = model.run(None, {input_name: input_data})[0]
+        probabilities = softmax(predictions)
+        pred_indices = np.flip(np.argsort\
+                        (probabilities, axis=1)[:, -args.top_predictions:], 1)
+        probabilities = np.take_along_axis(probabilities, pred_indices, 1) * 100
     return probabilities, pred_indices
 
 
@@ -46,23 +55,25 @@ def display_results(data, predictions):
         print(f"\n file {file} prediction: {prediction}")
 
     
-def export_to_onnx(model):
-    sample_input = torch.zeros((1, 3, 224, 224))
-    onnx.export(model, sample_input, 'checkpoints/model.onnx')
-
-
-def prepare_model():
-    if args.model_type == 'pytorch':
+def prepare_model(model_type):
+    if model_type == 'pytorch':
         model = models.resnet18(pretrained=True)
+    else:
+        model = ort.InferenceSession('checkpoints/model.onnx')
     return model
     
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    imagenet_classes = read_imagenet_classnames(\
-        os.path.join(args.data, "imagenet_classnames.txt"))
-    model = prepare_model()
-    data = preprocess_data(args.data)
-    predictions = run_inference(model, data)
-    display_results(data, predictions)
+    if args.export_model:
+        model = prepare_model("pytorch")
+        export_to_onnx(model)
+    else:
+        args.onnx = True if args.model_type == "onnx" else False
+        imagenet_classes = read_imagenet_classnames(\
+            os.path.join(args.data, "imagenet_classnames.txt"))
+        model = prepare_model(args.model_type)
+        data = preprocess_data(args.data)
+        predictions = run_inference(model, data, args.onnx)
+        display_results(data, predictions)
     
--- utils.py
+++ utils.py
@@ -1,5 +1,15 @@
 import os
 import json
+import torch
+import numpy as np
+import onnx
+import torch.onnx as torchonnx
+
+def softmax(x):
+    _max = np.max(x, axis=1).reshape(-1, 1)
+    x = x - _max
+    x = np.exp(x)
+    return x/np.sum(x, axis=1, keepdims=True)
 
 def load_json(path: str):
     """Loads json file"""
@@ -17,3 +27,11 @@ def read_imagenet_classnames(path:str):
     for i in temp:
         classes.append([k.strip(" ") for k in temp[i]])
     return classes
+
+def export_to_onnx(model):
+    model_path = 'checkpoints/model.onnx'
+    sample_input = torch.randn((1, 3, 256, 256))
+    torchonnx.export(model, sample_input, model_path)
+    model = onnx.load(model_path)
+    model.graph.input[0].type.tensor_type.shape.dim[0].dim_param = '?'
+    onnx.save(model, model_path)
